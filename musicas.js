79e2a94196173c479b894b679b169b4e927cc8d7
MxSGameJPS <mxsgamejps@gmail.com>
2025-06-08 16:22:23 -0300

    Atualização de lista

--- musicas.js
+++ musicas.js
@@ -4,77 +4,77 @@ const musicas = [
     "titulo": "Só Pagodes e Sambas Top 2025 🎤 Ferrugem, Dilsinho, Thiaguinho, Ludmilla, Iza e Mais",
     "artista": "Só Pagodes e Sambas",
     "estilo": "Pagode / Samba",
-    "url": "https://www.youtube.com/watch?v=NJjIuBjDKO8"
+    "url": "/musicas/Só Pagodes e Sambas Top 2025 🎤 Ferrugem, Dilsinho, Thiaguinho, Ludmilla, Iza e Mais [NJjIuBjDKO8].mp3"
   },
   {
     "id": 2,
     "titulo": "Melhores Musicas 2025 🎼 Sertanejo 2025 Mais Tocadas 🎼 As Melhores Do Sertanejo Universitário",
     "artista": "Sertanejo Universitário",
     "estilo": "Sertanejo",
-    "url": "https://www.youtube.com/watch?v=bBZl9aw_3pE"
+    "url": "/musicas/Melhores Musicas 2025 🎼 Sertanejo 2025 Mais Tocadas 🎼 As Melhores Do Sertanejo Universitário [bBZl9aw_3pE].mp3"
   },
   {
     "id": 3,
     "titulo": "Metallica, Queen, Nirvana, Guns N Roses, Bon Jovi, ACDC 🔥 Best Classic Rock Songs 70s 80s 90s",
     "artista": "Various Artists",
     "estilo": "Rock Clássico",
-    "url": "https://www.youtube.com/watch?v=aeBmia24CQQ"
+    "url": "/musicas/Metallica, Queen, Nirvana, Guns N Roses, Bon Jovi, ACDC 🔥 Best Classic Rock Songs 70s 80s 90s [aeBmia24CQQ].mp3"
   },
   {
     "id": 4,
     "titulo": "As Mais Tocadas Hungria As Melhores Hungria Hip Hop",
     "artista": "Humgria Hip Hop",
     "estilo": "Hip Hop",
-    "url": "https://www.youtube.com/watch?v=WVZOU0sf1gQ"
+    "url": "/musicas/As Mais Tocadas Hungria As Melhores Hungria Hip Hop [2b1k3g4j5h6i].mp3"
   },
   {
     "id": 5,
     "titulo": "Músicas MPB Para Relaxar - As Melhores MPB de Todos os Tempos - Maria Gadú, Kell Smith, Sandy",
     "artista": "MPB Relax",
     "estilo": "MPB",
-    "url": "https://www.youtube.com/watch?v=HmHKswwQsRE"
+    "url": "/musicas/Músicas MPB Para Relaxar - As Melhores MPB de Todos os Tempos - Maria Gadú, Kell Smith, Sandy [HmHKswwQsRE].mp3"
   },
   {
     "id": 6,
     "titulo": "Rock Nacional - Grandes Hits para ouvir em 2024 ",
     "artista": "Rock Nacional",
     "estilo": "Rock Nacional",
-    "url": "https://www.youtube.com/watch?v=4LrudHsth54"
+    "url": "/musicas/Rock Nacional - Grandes Hits para ouvir em 2024 🎸 [4LrudHsth54].mp3"
   },
   {
     "id": 7,
     "titulo": "Música de trabalho produtiva — Deep Focus Mix para programação e codificação",
     "artista": "Focus Music",
     "estilo": "Eletrônica / Focus Session",
-    "url": "https://www.youtube.com/watch?v=C55zjPlqdYw"
+    "url": "/musicas/Productive Work Music — Deep Focus Mix for Programming, Coding [C55zjPlqdYw].mp3"
   },
   {
     "id": 8,
     "titulo": "MÚSICA ELETRÔNICA 2024 🔥 AS MÚSICAS ELETRÔNICAS MAIS TOCADAS 🔥 Alok, Vintage Culture & David Guetta",
     "artista": "Various Artists",
     "estilo": "Eletrônica",
-    "url": "https://www.youtube.com/watch?v=kIfDEwUp4fw"
+    "url": "/musicas/MÚSICA ELETRÔNICA 2024 🔥 AS MÚSICAS ELETRÔNICAS MAIS TOCADAS 🔥 Alok, Vintage Culture & David Guetta [kIfDEwUp4fw].mp3"
   },
   {
     "id": 9,
     "titulo": "Sertanejo Universitário de 2010 a 2019 | Playlist com as melhores ",
     "artista": "Sertanejo Universitário",
     "estilo": "Sertanejo Universitário",
-    "url": "https://www.youtube.com/watch?v=I9ffMbKM4xA"
+    "url": "/musicas/Sertanejo Universitário de 2010 a 2019 _ Playlist com as melhores 🤠 [I9ffMbKM4xA].mp3"
   },
   {
     "id": 10,
     "titulo": "FUNK DO TIK TOK 2025 - MÚSICAS DO TIKTOK 2025 - FUNK ATUALIZADO - OS MELHORES MAIS TOCADOS",
     "artista": "Funk TikTok",
     "estilo": "Funk",
-    "url": "https://www.youtube.com/watch?v=Xwxusv-Dw3g"
+    "url": "/musicas/FUNK DO TIK TOK 2025 - MÚSICAS DO TIKTOK 2025 - FUNK ATUALIZADO - OS MELHORES MAIS TOCADOS [Xwxusv-Dw3g].mp3"
   },
   {
     "id": 100,
     "titulo": "Pink Floyd – Marooned (Instrumental)",
     "artista": "Pink Floyd",
     "estilo": "Rock Clássico Instrumental",
-    "url": "https://www.youtube.com/watch?v=DQvG2SMVl84"
+    "url": "/musicas/Pink Floyd - One of These Days.mp3"
   }
 ];
 
